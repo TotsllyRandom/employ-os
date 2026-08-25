d271e297c495f5c9faa332eebec1fce299d1c2de
Totally_RandomYT <68128408+TotsllyRandom@users.noreply.github.com>
2026-08-24 23:30:26 -0500

    Added basic window structure

---
+++ scripts/Globals/playerData.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var playerModFolder: FileAccess
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+## TO DO:
+# make player folder exist
+# add desktops folder to it
+# func to return random desktop on request as image
